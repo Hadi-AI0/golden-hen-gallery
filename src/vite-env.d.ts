97cb3608c2d1268e9a9effca6be910504c7fb911
Hadi-AI0 <118402547+Hadi-AI0@users.noreply.github.com>
2026-07-20 10:14:57 +0300

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" />
+
+// Google Analytics environment variable
+interface ImportMetaEnv {
+  readonly VITE_GA_MEASUREMENT_ID?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+// Image imports
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.gif';
+declare module '*.webp';
+
+// CSS imports
+declare module '*.css';
